5cce94016613ae730712df601811163feaac4d9c
sienlonglim <limsienlong@gmail.com>
2024-09-09 02:13:19 +0800

    Working dbt cli in dagster

--- dagster_elt/dbt/definitions.py
+++ dagster_elt/dbt/definitions.py
@@ -1,13 +1,12 @@
 from dagster import Definitions
-from dagster_dbt import DbtCliResource
 
 from .assets import build_all_assets
-from ..dagster_utils.constants import DBT_PROJECT_DIR
+from ..dagster_utils.resources import dbt_resource
 
 
 defs = Definitions(
     assets=[build_all_assets],
     resources={
-        "dbt": DbtCliResource(project_dir=DBT_PROJECT_DIR),
+        "dbt": dbt_resource,
     }
 )
